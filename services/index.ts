225eb3b53d2cd3ec83952eb976593053bbc50261
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-09-23 23:50:28 +0300

    .

--- services/index.ts
+++ services/index.ts
@@ -1,8 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-export const API_URL = 'http://193.227.240.235/api/';
-export const API_DOMAIN = 'http://193.227.240.235/';
+export const API_URL = "http://193.227.240.235/api/";
+export const API_DOMAIN = "http://193.227.240.235/";
 
 axios.defaults.baseURL = API_URL;
-
